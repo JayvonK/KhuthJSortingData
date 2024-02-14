87b8fc3d4d8a704be1393d062c45e45370611f6f
JayvonK <jaykhuth@gmail.com>
2024-02-14 14:02:24 -0800

    save

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,109 @@
 import { createRow } from "./createRow.js";
+import { nextCreate, backCreate } from "./nextPrev.js";
 
 let usersDiv1 = document.getElementById("usersDiv1");
+let btn10 = document.getElementById("btn10");
+let btn20 = document.getElementById("btn20");
+let btn30 = document.getElementById("btn30");
+let btn40 = document.getElementById("btn40");
+let btn50 = document.getElementById("btn50");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+let num = 10;
+let count = -1;
+
 
 const getUsers = async () => {
     const promise = await fetch('../data/data.json');
     const data = await promise.json();
-    console.log(data.People);
     return data.People;
 }
-
-getUsers();
 
 const initialCreate = async () => {
     let arr = await getUsers();
     usersDiv1.innerHTML = "";
-    for(let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
         createRow(arr[i].Id, arr[i].FirstName, arr[i].LastName, arr[i].Email, arr[i].Height, arr[i].Age);
     }
-} 
+}
 
 initialCreate();
+
+btn10.addEventListener('click', async () => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+        createRow(arr[i].Id, arr[i].FirstName, arr[i].LastName, arr[i].Email, arr[i].Height, arr[i].Age);
+    }
+    num = 10;
+    count = -1;
+})
+
+btn20.addEventListener('click', async () => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+    for (let i = 0; i < 20; i++) {
+        createRow(arr[i].Id, arr[i].FirstName, arr[i].LastName, arr[i].Email, arr[i].Height, arr[i].Age);
+    }
+    num = 20;
+    count = -1;
+})
+
+btn30.addEventListener('click', async () => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+    for (let i = 0; i < 30; i++) {
+        createRow(arr[i].Id, arr[i].FirstName, arr[i].LastName, arr[i].Email, arr[i].Height, arr[i].Age);
+    }
+    num = 30;
+    count = -1;
+})
+
+btn40.addEventListener('click', async () => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+    for (let i = 0; i < 40; i++) {
+        createRow(arr[i].Id, arr[i].FirstName, arr[i].LastName, arr[i].Email, arr[i].Height, arr[i].Age);
+    }
+    num = 40;
+    count = -1;
+})
+
+btn50.addEventListener('click', async () => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+    for (let i = 0; i < 50; i++) {
+        createRow(arr[i].Id, arr[i].FirstName, arr[i].LastName, arr[i].Email, arr[i].Height, arr[i].Age);
+    }
+    num = 50;
+    count = -1;
+})
+
+
+
+
+nextBtn.addEventListener('click', async () => {
+    count += num;
+    if(count > 98){
+        count = -1;
+    }
+    await nextCreate(num, count);
+    
+    
+    console.log(count);
+})
+
+backBtn.addEventListener('click', async () => {
+    let arr = await getUsers();
+    count -= num;
+    if(count < -1){
+        count = arr.length - 1 - num;
+    }
+    await nextCreate(num, count); 
+    console.log(count);
+})
+
 
 
 export { getUsers, usersDiv1 };
---
+++ scripts/nextPrev.js
@@ -0,0 +1,30 @@
+import { getUsers } from "./app.js";
+import { createRow } from "./createRow.js";
+
+const nextCreate = async (num, count) => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+
+    for(let i = 0; i < num; i++){
+        if(count < arr.length - 1){
+            count++;
+           createRow(arr[count].Id, arr[count].FirstName, arr[count].LastName, arr[count].Email, arr[count].Height, arr[count].Age);
+           console.log("created"); 
+        }
+    }
+}
+
+const backCreate = async (num, count) => {
+    let arr = await getUsers();
+    usersDiv1.innerHTML = "";
+
+    for(let i = num; i > 0; i--){
+        if(count > 0){
+            count--;
+           createRow(arr[count].Id, arr[count].FirstName, arr[count].LastName, arr[count].Email, arr[count].Height, arr[count].Age);
+           console.log("created"); 
+        }
+    }
+}
+
+export  { nextCreate, backCreate}
